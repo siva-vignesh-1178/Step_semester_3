a6206a8c3a6931e9b8f400617433048770b8b82b
sivavignesh-960 <sp8224@srmist.edu.in>
2026-09-12 17:58:49 +0530

    WordReversalEncoder.java

---
+++ WordReversalEncoder.java
@@ -0,0 +1,32 @@
+public class WordReversalEncoder.java {
+
+    static String reverseEachWord(String sentence) {
+
+        String[] words = sentence.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+
+            StringBuilder word =
+                new StringBuilder(words[i]);
+
+            word.reverse();
+
+            result.append(word);
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            reverseEachWord("hello club")
+        );
+    }
+}
